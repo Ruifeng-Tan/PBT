43f19aa6b60c19cea2c97edabb5fd48c1b8f097f
Hong Weixiang <1070446604@qq.com>
2025-03-13 17:51:49 +0800

    update prompt

---
+++ Prompts/XJTU_protocol_prompt.py
@@ -0,0 +1,31 @@
+class XJTU_protocol_prompt():
+    Protocols_prompts = {
+        1.0:
+            f"Battery specifications: The data comes from a lithium-ion battery in a format of 18650 cylindrical battery. "
+            f"Its positive electrode is Li(Ni0.5Co0.2Mn0.3)O2 (NCM523). "
+            f"Its negative electrode is graphite. "
+            f"The electrolyte formula is unknown. "
+            f"The battery manufacturer is LISHEN. "
+            f"The nominal capacity is 2 Ah. "
+            f"Operating condition: The working history of this battery is just after formation. "
+            f"The working ambient temperature of this battery is 20 degree Celsius. "
+            f"In the cycling, the battery was charged at a constant current of 2 C until reaching 4.2 V, "
+            f"and then 4.2 V was sustained until the current dropped to 0.05 C. And then rest for 5 minutes. "
+            f"The battery was then discharged at a constant current of 1 C until reaching 2.5 V, "
+            f"and then rest for 5 minutes. "
+            f"The cycling state-of-charge of this battery ranges from 0% to 100%.\n",
+        2.0:
+            f"Battery specifications: The data comes from a lithium-ion battery in a format of 18650 cylindrical battery. "
+            f"Its positive electrode is Li(Ni0.5Co0.2Mn0.3)O2 (NCM523). "
+            f"Its negative electrode is graphite. "
+            f"The electrolyte formula is unknown. "
+            f"The battery manufacturer is LISHEN. "
+            f"The nominal capacity is 2 Ah. "
+            f"Operating condition: The working history of this battery is just after formation. "
+            f"The working ambient temperature of this battery is 20 degree Celsius. "
+            f"In the cycling, the battery was charged at a constant current of 3 C until reaching 4.2 V, "
+            f"and then 4.2 V was sustained until the current dropped to 0.05 C. And then rest for 5 minutes. "
+            f"The battery was then discharged at a constant current of 1 C until reaching 2.5 V, "
+            f"and then rest for 5 minutes. "
+            f"The cycling state-of-charge of this battery ranges from 0% to 100%.\n"
+    }
