0c1c238b5f91a6dfd5b055f313098d0c95a824bf
w824449964 <824449964@qq.com>
2025-03-21 13:32:30 +0800

    add dVdQ model

--- run_accerlerate_BatteryLifeLLM2.py
+++ run_accerlerate_BatteryLifeLLM2.py
@@ -10,8 +10,7 @@ from utils.tools import train_model_course, get_parameter_number, is_training_la
 from utils.losses import bmc_loss, Battery_life_alignment_CL_loss, DG_loss, Alignment_loss
 from transformers import LlamaModel, LlamaTokenizer, LlamaForCausalLM, AutoConfig
 from BatteryLifeLLMUtils.configuration_BatteryLifeLLM import BatteryElectrochemicalConfig, BatteryLifeConfig
-from models import BatteryMoE_Gating_Imp, BatteryMoE_baseline, BatteryMoE_Gating_Imp, \
-    BatteryMoE_baseline_Gating, BatteryMoE_v1_casual, BatteryMoE_v1, BatteryMoE_Gating
+from models import BatteryMoE_Gating_allow_noDKP, BatteryMoE_Gating_allow_noDKP, BatteryMoE_baseline, BatteryMoE_baseline_Gating, BatteryMoE_v1_casual, BatteryMoE_Gating_dVdQ, BatteryMoE_Gating
 import wandb
 from peft import LoraConfig, PeftModel, get_peft_model, prepare_model_for_kbit_training, AdaLoraConfig
 from data_provider.data_factory import data_provider_LLMv2
@@ -225,12 +224,12 @@ for ii in range(args.itr):
         model_ec_config = BatteryElectrochemicalConfig(args.__dict__)
         model_text_config = AutoConfig.from_pretrained(args.LLM_path)
         model_config = BatteryLifeConfig(model_ec_config, model_text_config)
-        model = BatteryMoE_Gating_Imp.Model(model_config)
-    elif args.model == 'BatteryMoE_v1':
+        model = BatteryMoE_Gating_allow_noDKP.Model(model_config)
+    elif args.model == 'BatteryMoE_Gating_dVdQ':
         model_ec_config = BatteryElectrochemicalConfig(args.__dict__)
         model_text_config = AutoConfig.from_pretrained(args.LLM_path)
         model_config = BatteryLifeConfig(model_ec_config, model_text_config)
-        model = BatteryMoE_v1.Model(model_config)
+        model = BatteryMoE_Gating_dVdQ.Model(model_config)
     elif args.model == 'BatteryMoE_Gating':
         model_ec_config = BatteryElectrochemicalConfig(args.__dict__)
         model_text_config = AutoConfig.from_pretrained(args.LLM_path)
@@ -246,11 +245,11 @@ for ii in range(args.itr):
         model_text_config = AutoConfig.from_pretrained(args.LLM_path)
         model_config = BatteryLifeConfig(model_ec_config, model_text_config)
         model = BatteryMoE_baseline.Model(model_config)
-    elif args.model == 'BatteryMoE_Gating_Imp':
+    elif args.model == 'BatteryMoE_Gating_allow_noDKP':
         model_ec_config = BatteryElectrochemicalConfig(args.__dict__)
         model_text_config = AutoConfig.from_pretrained(args.LLM_path)
         model_config = BatteryLifeConfig(model_ec_config, model_text_config)
-        model = BatteryMoE_Gating_Imp.Model(model_config)
+        model = BatteryMoE_Gating_allow_noDKP.Model(model_config)
     else:
         raise Exception('Not Implemented')
 
