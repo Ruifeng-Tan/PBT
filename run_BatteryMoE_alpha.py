9adeb792e9e4a8e04868264ebb2c6346cd60cdc6
Ruifeng Tan <824449964@qq.com>
2025-03-25 14:32:17 +0800

    rename baseline models

--- run_BatteryMoE_alpha.py
+++ run_BatteryMoE_alpha.py
@@ -10,7 +10,7 @@ from utils.tools import train_model_course, get_parameter_number, is_training_la
 from utils.losses import bmc_loss, Battery_life_alignment_CL_loss, DG_loss, Alignment_loss
 from transformers import LlamaModel, LlamaTokenizer, LlamaForCausalLM, AutoConfig
 from BatteryLifeLLMUtils.configuration_BatteryLifeLLM import BatteryElectrochemicalConfig, BatteryLifeConfig
-from models import BatteryMoE_Gating_GELU, BatteryMoE_Gating_ReLU, BatteryMoE_Gating_SwiGLU, BatteryMoE_baseline, BatteryMoE_Gating_GEGLU, BatteryMoE_Gating_Linear, BatteryMoE_Gating
+from models import BatteryMoE_Gating_GELU, BatteryMoE_Gating_ReLU, BatteryMoE_Gating_SwiGLU, BatteryMoE_Gating_GEGLU, BatteryMoE_Gating_Linear, BatteryMoE_Gating, baseline_CPTransformerMoE
 import wandb
 from peft import LoraConfig, PeftModel, get_peft_model, prepare_model_for_kbit_training, AdaLoraConfig
 from data_provider.data_factory import data_provider_LLMv2
@@ -240,11 +240,11 @@ for ii in range(args.itr):
         model_text_config = AutoConfig.from_pretrained(args.LLM_path)
         model_config = BatteryLifeConfig(model_ec_config, model_text_config)
         model = BatteryMoE_Gating_Linear.Model(model_config)
-    elif args.model == 'BatteryMoE_baseline':
+    elif args.model == 'baseline_CPTransformerMoE':
         model_ec_config = BatteryElectrochemicalConfig(args.__dict__)
         model_text_config = AutoConfig.from_pretrained(args.LLM_path)
         model_config = BatteryLifeConfig(model_ec_config, model_text_config)
-        model = BatteryMoE_baseline.Model(model_config)
+        model = baseline_CPTransformerMoE.Model(model_config)
     elif args.model == 'BatteryMoE_Gating_ReLU':
         model_ec_config = BatteryElectrochemicalConfig(args.__dict__)
         model_text_config = AutoConfig.from_pretrained(args.LLM_path)
