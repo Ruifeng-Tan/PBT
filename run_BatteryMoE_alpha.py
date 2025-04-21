b2fb4a1567a394275fc2eda24df63ed0e6f9e78e
w824449964 <824449964@qq.com>
2025-04-21 11:50:35 +0800

    update model

--- run_BatteryMoE_alpha.py
+++ run_BatteryMoE_alpha.py
@@ -10,7 +10,7 @@ from utils.tools import train_model_course, get_parameter_number, is_training_la
 from utils.losses import bmc_loss, Battery_life_alignment_CL_loss, DG_loss, Alignment_loss
 from transformers import LlamaModel, LlamaTokenizer, LlamaForCausalLM, AutoConfig
 from BatteryLifeLLMUtils.configuration_BatteryLifeLLM import BatteryElectrochemicalConfig, BatteryLifeConfig
-from models import BatteryMoE_DKPNorm, baseline_CPTransformerMoE, BatteryMoE_PESum, BatteryMoE_DKPNorm_v2, baseline_CPMLPMoE
+from models import BatteryMoE_DKPNorm, baseline_CPTransformerMoE, BatteryMoE_PESum_mask, BatteryMoE_DKPNorm_v2, baseline_CPMLPMoE
 import wandb
 from peft import LoraConfig, PeftModel, get_peft_model, prepare_model_for_kbit_training, AdaLoraConfig
 from data_provider.data_factory import data_provider_LLMv2
@@ -205,11 +205,11 @@ for ii in range(args.itr):
         model_text_config = AutoConfig.from_pretrained(args.LLM_path)
         model_config = BatteryLifeConfig(model_ec_config, model_text_config)
         model = baseline_CPTransformerMoE.Model(model_config)
-    elif args.model == 'BatteryMoE_PESum':
+    elif args.model == 'BatteryMoE_PESum_mask':
         model_ec_config = BatteryElectrochemicalConfig(args.__dict__)
         model_text_config = AutoConfig.from_pretrained(args.LLM_path)
         model_config = BatteryLifeConfig(model_ec_config, model_text_config)
-        model = BatteryMoE_PESum.Model(model_config)
+        model = BatteryMoE_PESum_mask.Model(model_config)
     elif args.model == 'BatteryMoE_DKPNorm':
         model_ec_config = BatteryElectrochemicalConfig(args.__dict__)
         model_text_config = AutoConfig.from_pretrained(args.LLM_path)
