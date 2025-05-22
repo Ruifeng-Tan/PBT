00638b1c000940b9fc460d53e208187003c5d131
w824449964 <824449964@qq.com>
2025-05-23 00:20:16 +0800

    update model

--- run_BatteryMoE_alpha.py
+++ run_BatteryMoE_alpha.py
@@ -10,7 +10,7 @@ from utils.tools import get_parameter_number
 from utils.losses import DG_loss, Alignment_loss, AverageRnCLoss, WeightedRnCLoss
 from transformers import LlamaModel, LlamaTokenizer, LlamaForCausalLM, AutoConfig
 from BatteryLifeLLMUtils.configuration_BatteryLifeLLM import BatteryElectrochemicalConfig, BatteryLifeConfig
-from models import BatteryMoE_Hyper_CropAugIMP45, BatteryMoE_Hyper_RMS, baseline_CPTransformerMoE, BatteryMoE_Hyper_CropAugIMP, baseline_CPMLPMoE
+from models import BatteryMoE_Hyper_FixCrop, BatteryMoE_Hyper_RMS, baseline_CPTransformerMoE, BatteryMoE_Hyper_CropAugIMP, baseline_CPMLPMoE
 import pickle
 import wandb
 from data_provider.data_factory import data_provider_LLMv2
@@ -232,11 +232,11 @@ for ii in range(args.itr):
         model_text_config = AutoConfig.from_pretrained(args.LLM_path)
         model_config = BatteryLifeConfig(model_ec_config, model_text_config)
         model = baseline_CPTransformerMoE.Model(model_config)
-    elif args.model == 'BatteryMoE_Hyper_CropAugIMP45':
+    elif args.model == 'BatteryMoE_Hyper_FixCrop':
         model_ec_config = BatteryElectrochemicalConfig(args.__dict__)
         model_text_config = AutoConfig.from_pretrained(args.LLM_path)
         model_config = BatteryLifeConfig(model_ec_config, model_text_config)
-        model = BatteryMoE_Hyper_CropAugIMP45.Model(model_config)
+        model = BatteryMoE_Hyper_FixCrop.Model(model_config)
     elif args.model == 'BatteryMoE_Hyper_RMS':
         model_ec_config = BatteryElectrochemicalConfig(args.__dict__)
         model_text_config = AutoConfig.from_pretrained(args.LLM_path)
