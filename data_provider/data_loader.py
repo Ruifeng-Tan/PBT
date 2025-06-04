5c8ec5d7f5cec406f6902a5afa7f456e9ef627d1
w824449964 <824449964@qq.com>
2025-06-04 20:50:35 +0800

    update

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -733,7 +733,7 @@ class Dataset_BatteryLifeLLM_original(Dataset):
             temperature_mask = list(temperature_mask)
 
             if file_name in self.format_json:
-                format = self.format_json[file_name][0]
+                format = '_'.join(self.format_json[file_name])
                 # if file_name.startswith('NA-ion'):
                 #     format = 'Na_cylindrical' # Na-ion cylindrical is not mixed with Li cylindrical
                 format_index = self.format2mask[format] if format in self.format2mask else np.arange(self.format_experts)
