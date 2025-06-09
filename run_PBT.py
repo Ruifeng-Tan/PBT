be21bea7462c9c75c0990c378bb248c045013312
w824449964 <824449964@qq.com>
2025-06-09 15:20:33 +0800

    update prompt

--- run_PBT.py
+++ run_PBT.py
@@ -290,7 +290,7 @@ for ii in range(args.itr):
     if accelerator.is_local_main_process:
         wandb.init(
         # set the wandb project where this run will be logged
-        project="PBT_aa",
+        project="PBT_fix",
         
         # track hyperparameters and run metadata
         config=args.__dict__,
