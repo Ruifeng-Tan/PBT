08ae46a7d396cf3687b160183d608fdfb8e5b26d
Hong Weixiang <1070446604@qq.com>
2025-07-13 17:07:36 +0800

    fix Tongji split

---
+++ check.py
@@ -0,0 +1,25 @@
+
+
+Tongji_train_files =  ['Tongji1_CY35-05_1--2.pkl', 'Tongji1_CY45-05_1--21.pkl', 'Tongji1_CY45-05_1--28.pkl', 'Tongji2_CY45-05_1--11.pkl', 'Tongji1_CY25-1_1--5.pkl', 'Tongji1_CY25-1_1--9.pkl', 'Tongji1_CY45-05_1--19.pkl', 'Tongji3_CY25-05_4--1.pkl', 'Tongji1_CY25-05_1--2.pkl', 'Tongji3_CY25-05_2--1.pkl', 'Tongji1_CY45-05_1--5.pkl', 'Tongji1_CY45-05_1--13.pkl', 'Tongji1_CY45-05_1--11.pkl', 'Tongji1_CY35-05_1--1.pkl', 'Tongji1_CY45-05_1--17.pkl', 'Tongji2_CY25-05_1--13.pkl', 'Tongji1_CY25-1_1--6.pkl', 'Tongji2_CY45-05_1--22.pkl', 'Tongji1_CY45-05_1--27.pkl', 'Tongji2_CY45-05_1--27.pkl', 'Tongji1_CY25-05_1--17.pkl', 'Tongji2_CY45-05_1--9.pkl', 'Tongji3_CY25-05_2--2.pkl', 'Tongji1_CY45-05_1--18.pkl', 'Tongji3_CY25-05_4--2.pkl', 'Tongji1_CY45-05_1--20.pkl', 'Tongji1_CY45-05_1--1.pkl', 'Tongji2_CY45-05_1--23.pkl', 'Tongji2_CY25-05_1--5.pkl', 'Tongji2_CY45-05_1--25.pkl', 'Tongji1_CY25-05_1--4.pkl', 'Tongji1_CY25-05_1--12.pkl', 'Tongji1_CY25-05_1--16.pkl', 'Tongji1_CY25-05_1--13.pkl', 'Tongji2_CY25-05_1--17.pkl', 'Tongji1_CY45-05_1--7.pkl', 'Tongji1_CY25-025_1--5.pkl', 'Tongji2_CY25-05_1--12.pkl', 'Tongji1_CY45-05_1--6.pkl', 'Tongji3_CY25-05_1--2.pkl', 'Tongji3_CY25-05_2--3.pkl', 'Tongji2_CY45-05_1--16.pkl', 'Tongji1_CY25-05_1--15.pkl', 'Tongji1_CY25-025_1--3.pkl', 'Tongji2_CY45-05_1--7.pkl', 'Tongji2_CY45-05_1--20.pkl', 'Tongji1_CY45-05_1--23.pkl', 'Tongji2_CY25-05_1--15.pkl', 'Tongji2_CY35-05_1--4.pkl', 'Tongji1_CY45-05_1--12.pkl', 'Tongji2_CY45-05_1--15.pkl', 'Tongji1_CY25-05_1--5.pkl', 'Tongji2_CY35-05_1--3.pkl', 'Tongji1_CY25-05_1--14.pkl', 'Tongji1_CY45-05_1--26.pkl', 'Tongji1_CY45-05_1--22.pkl', 'Tongji2_CY45-05_1--24.pkl', 'Tongji1_CY25-05_1--18.pkl', 'Tongji1_CY45-05_1--9.pkl', 'Tongji1_CY45-05_1--24.pkl', 'Tongji1_CY25-1_1--4.pkl', 'Tongji2_CY45-05_1--26.pkl', 'Tongji3_CY25-05_1--3.pkl', 'Tongji1_CY45-05_1--15.pkl', 'Tongji1_CY25-025_1--2.pkl', 'Tongji2_CY45-05_1--14.pkl']
+Tongji_val_files = ['Tongji1_CY25-1_1--3.pkl', 'Tongji1_CY25-05_1--3.pkl', 'Tongji1_CY45-05_1--10.pkl', 'Tongji1_CY45-05_1--14.pkl', 'Tongji2_CY45-05_1--21.pkl', 'Tongji2_CY45-05_1--8.pkl', 'Tongji2_CY25-05_1--8.pkl', 'Tongji2_CY45-05_1--18.pkl', 'Tongji1_CY25-1_1--8.pkl', 'Tongji1_CY45-05_1--8.pkl', 'Tongji2_CY45-05_1--10.pkl', 'Tongji1_CY45-05_1--16.pkl', 'Tongji2_CY45-05_1--1.pkl', 'Tongji1_CY25-05_1--10.pkl', 'Tongji1_CY25-025_1--4.pkl', 'Tongji2_CY25-05_1--16.pkl', 'Tongji1_CY25-05_1--11.pkl', 'Tongji2_CY45-05_1--12.pkl', 'Tongji2_CY25-05_1--10.pkl', 'Tongji1_CY25-025_1--1.pkl', 'Tongji1_CY25-05_1--7.pkl']
+Tongji_test_files =['Tongji1_CY45-05_1--2.pkl', 'Tongji1_CY45-05_1--25.pkl', 'Tongji2_CY25-05_1--9.pkl', 'Tongji1_CY25-1_1--1.pkl', 'Tongji1_CY25-05_1--19.pkl', 'Tongji1_CY25-1_1--2.pkl', 'Tongji2_CY45-05_1--17.pkl', 'Tongji1_CY25-025_1--7.pkl', 'Tongji2_CY45-05_1--19.pkl', 'Tongji2_CY45-05_1--13.pkl', 'Tongji1_CY25-025_1--6.pkl', 'Tongji2_CY35-05_1--2.pkl', 'Tongji2_CY45-05_1--2.pkl', 'Tongji1_CY25-05_1--1.pkl', 'Tongji3_CY25-05_4--3.pkl', 'Tongji3_CY25-05_1--1.pkl', 'Tongji1_CY25-1_1--7.pkl', 'Tongji2_CY45-05_1--28.pkl', 'Tongji2_CY35-05_1--1.pkl', 'Tongji2_CY25-05_1--2.pkl', 'Tongji1_CY25-05_1--6.pkl']
+all_files = Tongji_train_files + Tongji_val_files + Tongji_test_files
+
+prefix = 'Tongji3_CY25-05_4'
+trains = []
+for file in Tongji_train_files:
+    if file.startswith(prefix):
+        trains.append(file)
+print('trains', trains)
+
+vals = []
+for file in Tongji_val_files:
+    if file.startswith(prefix):
+        vals.append(file)
+print('vals', vals)
+
+tests = []
+for file in Tongji_test_files:
+    if file.startswith(prefix):
+        tests.append(file)
+print('tests', tests)
