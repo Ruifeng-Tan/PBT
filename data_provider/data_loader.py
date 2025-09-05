e3187d46d079ad59f134e46fd33a98687b646cd8
Ruifeng Tan <824449964@qq.com>
2025-09-05 08:58:40 +0800

    fix the split

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -457,8 +457,16 @@ class Dataset_PBT(Dataset):
             self.train_files = split_recorder.ISU_ILCC_train_files
             self.val_files = split_recorder.ISU_ILCC_val_delG49C1_files
             self.test_files = split_recorder.ISU_ILCC_val_delG49C1_files
-        elif self.dataset == 'MIX_large_ablation_50p':
-            self.train_files = split_recorder.MIX_large_reduced_train_50p_files
+        elif self.dataset == 'MIX_large_ablation_50p' and self.args.seed == 2021:
+            self.train_files = split_recorder.MIX_large_reduced_train_50p_files_2021
+            self.val_files = split_recorder.MIX_large_val_files 
+            self.test_files = split_recorder.MIX_large_test_files
+        elif self.dataset == 'MIX_large_ablation_50p' and self.args.seed == 42:
+            self.train_files = split_recorder.MIX_large_reduced_train_50p_files_42
+            self.val_files = split_recorder.MIX_large_val_files 
+            self.test_files = split_recorder.MIX_large_test_files
+        elif self.dataset == 'MIX_large_ablation_50p' and self.args.seed == 2024:
+            self.train_files = split_recorder.MIX_large_reduced_train_50p_files_2024
             self.val_files = split_recorder.MIX_large_val_files 
             self.test_files = split_recorder.MIX_large_test_files
         else:
