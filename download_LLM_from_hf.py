e8c65608770b1f6719a3d4ac398e440b0979e395
Ruifeng Tan（谭瑞锋） <50574059+Ruifeng-Tan@users.noreply.github.com>
2026-04-24 15:06:11 +0800

    Update the Qwen3 embedding name

--- download_LLM_from_hf.py
+++ download_LLM_from_hf.py
@@ -1,4 +1,4 @@
 # Load model directly
 from huggingface_hub import snapshot_download
 
-snapshot_download(repo_id="Qwen/Qwen3-0.6B", cache_dir='/data/LLMs/')
+snapshot_download(repo_id="Qwen/Qwen3-Embedding-0.6B", cache_dir='/data/LLMs/')
